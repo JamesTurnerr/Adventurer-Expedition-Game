ac34f94f2a7f07829f08c942c9dfe635c8671127
diegobecker97 <thediegobecker@gmail.com>
2026-05-02 21:19:48 +1200

    made the new startup screen work

---
+++ src/main/java/seng201/team0/gui/OLD/SetupController.java
@@ -0,0 +1,87 @@
+/*
+package seng201.team0.gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import seng201.team0.models.Adventurer;
+import seng201.team0.models.Item;
+import seng201.team0.services.SetupService;
+import seng201.team0.services.GuiService;
+import seng201.team0.models.UserData;
+
+public class SetupController implements Initializable {
+    @FXML
+    private TextField guildInputTextField, expeditionInputTextField;
+    @FXML
+    private Button startButton;
+    @FXML
+    private MenuButton difficultyMenuButton;
+    @FXML
+    private MenuItem easyMenuItem, normalMenuItem, hardMenuItem;
+    @FXML
+    private ListView<Adventurer> chosenAdventurersListView;
+    @FXML
+    private ListView<Adventurer> availableAdventurersListView;
+    @FXML
+    private Button chooseAdventurerButton, unchooseAdventurerButton;
+
+    private final SetupService setupService = new SetupService();
+    private final GuiService guiService = new GuiService();
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1)
+    {
+        setupService.fillStarterAdventurerList(availableAdventurersListView, 5);
+    }
+    @FXML
+    private void easyModeSelected() {
+        difficultyMenuButton.setText("Easy");
+    }
+    @FXML
+    private void normalModeSelected() {
+        difficultyMenuButton.setText("Normal");
+    }
+    @FXML
+    private void hardModeSelected() {
+        difficultyMenuButton.setText("Hard");
+    }
+    @FXML
+    private void startButtonClicked() throws IOException {
+        if (setupService.checkInputs(expeditionInputTextField.getText(), guildInputTextField.getText(), difficultyMenuButton.getText(), chosenAdventurersListView.getItems()))
+        {
+            ArrayList<Adventurer> chosenAdventurersList = new ArrayList<Adventurer>(chosenAdventurersListView.getItems());
+            UserData userData = new UserData(chosenAdventurersList, difficultyMenuButton.getText(), guildInputTextField.getText(), Integer.parseInt(expeditionInputTextField.getText()));
+            userData.addItem(Item.HEALTH_POTION);
+
+            Stage stage = (Stage) startButton.getScene().getWindow();
+            guiService.switchWindow(stage, "/fxml/main_screen.fxml");
+        }
+
+    }
+    @FXML
+    private void chooseAdventurerClicked() {
+        Adventurer selectedAdventurer = availableAdventurersListView.getSelectionModel().getSelectedItem();
+        if (selectedAdventurer != null) {
+            chosenAdventurersListView.getItems().add(selectedAdventurer);
+            availableAdventurersListView.getItems().remove(selectedAdventurer);
+        }
+    }
+    @FXML
+    private void unchooseAdventurerClicked() {
+        Adventurer selectedAdventurer = chosenAdventurersListView.getSelectionModel().getSelectedItem();
+        if (selectedAdventurer != null) {
+            availableAdventurersListView.getItems().add(chosenAdventurersListView.getSelectionModel().getSelectedItem());
+            chosenAdventurersListView.getItems().remove(chosenAdventurersListView.getSelectionModel().getSelectedItem());
+        }
+    }
+}
+
+*/
--- src/main/java/seng201/team0/gui/SetupController.java
+++ src/main/java/seng201/team0/gui/SetupController.java
@@ -4,82 +4,142 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+import seng201.team0.models.Adventurer;
+import seng201.team0.models.Item;
+import seng201.team0.models.UserData;
+import seng201.team0.services.GuiService;
+import seng201.team0.services.SetupService;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
-import seng201.team0.models.Adventurer;
-import seng201.team0.models.Item;
-import seng201.team0.services.SetupService;
-import seng201.team0.services.GuiService;
-import seng201.team0.models.UserData;
-
 public class SetupController implements Initializable {
-    @FXML
-    private TextField guildInputTextField, expeditionInputTextField;
-    @FXML
-    private Button startButton;
-    @FXML
-    private MenuButton difficultyMenuButton;
-    @FXML
-    private MenuItem easyMenuItem, normalMenuItem, hardMenuItem;
-    @FXML
-    private ListView<Adventurer> chosenAdventurersListView;
-    @FXML
-    private ListView<Adventurer> availableAdventurersListView;
-    @FXML
-    private Button chooseAdventurerButton, unchooseAdventurerButton;
+    @FXML private TextField guildInputTextField, expeditionInputTextField;
+    @FXML private Button startButton;
+    @FXML private MenuButton difficultyMenuButton;
+    @FXML private MenuItem easyMenuItem, normalMenuItem, hardMenuItem;
+    @FXML private ListView<Adventurer> availableAdventurersListView;
+    @FXML private Button chooseAdventurerButton, unchooseAdventurerButton;
 
     private final SetupService setupService = new SetupService();
     private final GuiService guiService = new GuiService();
 
-    @Override
-    public void initialize(URL arg0, ResourceBundle arg1)
-    {
-        setupService.fillStarterAdventurerList(availableAdventurersListView, 5);
-    }
-    @FXML
-    private void easyModeSelected() {
-        difficultyMenuButton.setText("Easy");
-    }
-    @FXML
-    private void normalModeSelected() {
-        difficultyMenuButton.setText("Normal");
-    }
-    @FXML
-    private void hardModeSelected() {
-        difficultyMenuButton.setText("Hard");
-    }
-    @FXML
-    private void startButtonClicked() throws IOException {
-        if (setupService.checkInputs(expeditionInputTextField.getText(), guildInputTextField.getText(), difficultyMenuButton.getText(), chosenAdventurersListView.getItems()))
-        {
-            ArrayList<Adventurer> chosenAdventurersList = new ArrayList<Adventurer>(chosenAdventurersListView.getItems());
-            UserData userData = new UserData(chosenAdventurersList, difficultyMenuButton.getText(), guildInputTextField.getText(), Integer.parseInt(expeditionInputTextField.getText()));
-            userData.addItem(Item.HEALTH_POTION);
+    @FXML private Button slot1Button;
+    @FXML private Button slot2Button;
+    @FXML private Button slot3Button;
+    private List<Button> selectedAdventurerButtons;
 
-            Stage stage = (Stage) startButton.getScene().getWindow();
-            guiService.switchWindow(stage, "/fxml/main_screen.fxml");
+    private List<Adventurer> mainParty;
+
+    private int selectedSlotIndex = -1;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setupService.fillStarterAdventurerList(availableAdventurersListView, 5);
+
+        selectedAdventurerButtons = List.of(slot1Button, slot2Button, slot3Button);
+
+        // sets the slot index
+        for (int i = 0; i < selectedAdventurerButtons.size(); i++) {
+            int index = i;
+
+            selectedAdventurerButtons.get(i).setOnAction(e -> {
+                selectedSlotIndex = index;
+                System.out.println("Selected slot: " + selectedSlotIndex);
+            });
         }
 
+        mainParty = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            mainParty.add(null);
+        }
     }
+    @FXML private void easyModeSelected() {difficultyMenuButton.setText("Easy");}
+    @FXML private void normalModeSelected() {difficultyMenuButton.setText("Normal");}
+    @FXML private void hardModeSelected() {difficultyMenuButton.setText("Hard");}
+
+    @FXML
+    private void startButtonClicked() throws IOException {
+        if (!setupService.checkInputs(
+                expeditionInputTextField.getText(),
+                guildInputTextField.getText(),
+                difficultyMenuButton.getText(),
+                mainParty
+        )) {
+            return;
+        }
+        ArrayList<Adventurer> chosenAdventurersList =
+                new ArrayList<>(mainParty);
+
+        UserData userData = new UserData(
+                chosenAdventurersList,
+                difficultyMenuButton.getText(),
+                guildInputTextField.getText(),
+                Integer.parseInt(expeditionInputTextField.getText())
+        );
+
+        userData.addItem(Item.HEALTH_POTION);
+
+        Stage stage = (Stage) startButton.getScene().getWindow();
+        guiService.switchWindow(stage, "/fxml/main_screen.fxml");
+    }
+
     @FXML
     private void chooseAdventurerClicked() {
         Adventurer selectedAdventurer = availableAdventurersListView.getSelectionModel().getSelectedItem();
-        if (selectedAdventurer != null) {
-            chosenAdventurersListView.getItems().add(selectedAdventurer);
-            availableAdventurersListView.getItems().remove(selectedAdventurer);
+        System.out.println(selectedAdventurer);
+
+
+
+        if (selectedSlotIndex == -1) {
+            System.out.println("nothing selected");
+            return;
         }
+
+        if (mainParty.size() >= selectedSlotIndex){
+            if ( mainParty.get(selectedSlotIndex) != null) {
+                System.out.println("already active");
+                return;
+            }
+        }
+        availableAdventurersListView.getItems().remove(selectedAdventurer);
+        mainParty.set(selectedSlotIndex, selectedAdventurer);
+        updateSlotButton(selectedSlotIndex, selectedAdventurer);
+        selectedSlotIndex = -1;
     }
+
     @FXML
     private void unchooseAdventurerClicked() {
-        Adventurer selectedAdventurer = chosenAdventurersListView.getSelectionModel().getSelectedItem();
-        if (selectedAdventurer != null) {
-            availableAdventurersListView.getItems().add(chosenAdventurersListView.getSelectionModel().getSelectedItem());
-            chosenAdventurersListView.getItems().remove(chosenAdventurersListView.getSelectionModel().getSelectedItem());
+        if (selectedSlotIndex == -1) {
+            System.out.println("nothing selected");
+            return;
+        }
+        Adventurer removed = mainParty.get(selectedSlotIndex);
+        if (removed == null) {
+            System.out.println("slot already empty");
+            return;
+        }
+        availableAdventurersListView.getItems().add(removed);
+        mainParty.set(selectedSlotIndex, null);
+        updateSlotButton(selectedSlotIndex, null);
+        selectedSlotIndex = -1;
+    }
+
+    private void UpdateStats(Adventurer adventurer){
+
+    }
+
+    private void updateSlotButton(int index, Adventurer adventurer) {
+        Button button = selectedAdventurerButtons.get(index);
+
+        if (adventurer == null) {
+            button.setText("Empty");
+        } else {
+            button.setText(adventurer.toString());
         }
     }
-}
 
+}
